d52080419911fb344893f327c4e63cf8d455cae0
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-19 14:32:31 +0500

    a

--- conf/urls.py
+++ conf/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for blogs project.
+URL configuration for conf project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-from users.urls import home_page_view, home3_page_view
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    path('helo', home3_page_view)
 ]
--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from users.views import home_page_view, home2_page_view, home3_page_view
-urlpatterns = [
-    path('hello/', home_page_view),
-    path("me/", home2_page_view),
-    path('helo/', home3_page_view)
-]
