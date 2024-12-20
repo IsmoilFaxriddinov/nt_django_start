b4fa4b2216edc397309ef6f8fc216eeacd9ff957
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-20 16:47:14 +0500

    a

--- homework/views.py
+++ homework/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home_page_view(request):
     return render(request, 'main.html')
+
+def about_page_view(request):
+    return render(request, 'about.html')
+
+def interests_page_view(request):
+    return render(request, 'interests.html')
+
+def contact_page_view(request):
+    return render(request, 'contact.html')
