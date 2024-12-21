cf577e0eca8aab210829108e4220b6dc4f349e57
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-21 10:10:56 +0500

    1

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from blogs.models import BlogsModel
+
 # Register your models here.
+
+admin.site.register(BlogsModel)
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+def BlogsModel(models.Model):
+    totle = models.CharField(max_length=50, null=True, blank=True, default=True)
+    content = models.TextField()
+    image = models.ImageField(upload_to="blogs")
+
+    class Meta:
+        db_table = 'blogs'
+    
