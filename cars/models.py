01b42f52f822db2f7e81f430488bcdca864e5c94
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-22 19:21:40 +0500

    a

--- cars/models.py
+++ cars/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 
 class CarsModel(models.Model):
-    ...
+    name = models.CharField(max_length=24, null=True)
+    description = models.TextField()
+    price = models.FloatField(default=0.0)
+    image = models.ImageField(upload_to='cars/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        db_table = 'cars'
