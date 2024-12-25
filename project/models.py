c481b96bc2753003b6309507d2a490ee1c57bb14
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-25 16:16:25 +0500

    a

--- project/models.py
+++ project/models.py
@@ -3,8 +3,6 @@ from django.db import models
 class TeacherModel(models.Model):
     name = models.CharField(max_length=255, null=True)
 
-
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
@@ -30,7 +28,8 @@ class StudentModel(models.Model):
 
 class GroupModel(models.Model):
     name = models.CharField(max_length=50)
-    teacher = models.ForeignKey(TeacherModel, on_delete=models.PROTECT)
+    teacher = models.ManyToManyField(TeacherModel)
+    students = models.ManyToManyField(StudentModel)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
@@ -39,11 +38,12 @@ class GroupModel(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = 'Student'
-        verbose_name_plural = 'Students'
+        verbose_name = 'Group'
+        verbose_name_plural = 'Groups'
 
 class LessonModel(models.Model):
     name = models.CharField(max_length=50)
+    groups = models.ForeignKey(GroupModel, on_delete=models.PROTECT)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
@@ -52,18 +52,19 @@ class LessonModel(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = 'Student'
-        verbose_name_plural = 'Students'
+        verbose_name = 'Lesson'
+        verbose_name_plural = 'Lessons'
 
 class HomeworkModel(models.Model):
     info = models.TextField()
+    lesson = models.OneToOneField(LessonModel, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.info
 
     class Meta:
-        verbose_name = 'Student'
-        verbose_name_plural = 'Students'
+        verbose_name = 'Homework'
+        verbose_name_plural = 'Homeworks'
