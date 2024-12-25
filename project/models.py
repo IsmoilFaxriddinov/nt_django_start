a8579e9fdf406bbac4f8e8ecce0cee6578cdb07c
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-25 16:06:51 +0500

    a

--- project/models.py
+++ project/models.py
@@ -1,25 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
-class StudentModel(models.Model):
-    name = models.CharField(max_length=50)
-    group = models.CharField(max_length=50)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Student'
-        verbose_name_plural = 'Students'
-
 class TeacherModel(models.Model):
     name = models.CharField(max_length=255, null=True)
-    student = models.IntegerField()
-    clas = models.ForeignKey(StudentModel, on_delete=models.CASCADE)
+
 
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -32,3 +15,55 @@ class TeacherModel(models.Model):
         verbose_name = 'Teacher'
         verbose_name_plural = 'Teachers'
 
+class StudentModel(models.Model):
+    name = models.CharField(max_length=50)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+class GroupModel(models.Model):
+    name = models.CharField(max_length=50)
+    teacher = models.ForeignKey(TeacherModel, on_delete=models.PROTECT)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+class LessonModel(models.Model):
+    name = models.CharField(max_length=50)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+class HomeworkModel(models.Model):
+    info = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
